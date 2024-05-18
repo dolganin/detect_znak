3e3aba470df7d1cfee89af92f1b650100efed924
Долганин Сергей <102363471+dolganin@users.noreply.github.com>
2024-05-18 10:17:12 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from detect_znak import pipeline
-from detect_znak.tools import unzip
-from detect_znak.image_loaders import DumpyImageLoader
-
-
-recogn = pipeline("multiline_number_plate_detection_and_reading",image_loader=DumpyImageLoader)
